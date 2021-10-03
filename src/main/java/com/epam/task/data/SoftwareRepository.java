2d6a6588c1c2aed9553dc85bd2b67532292c5d78
Edvard Krainiy <ekrayniy@inbox.ru>
2021-10-03 15:14:46 +0300

    Deleted unnecessary method.

--- src/main/java/com/epam/task/data/SoftwareRepository.java
+++ src/main/java/com/epam/task/data/SoftwareRepository.java
@@ -6,7 +6,6 @@ import com.epam.task.data.util.FileUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,7 +16,6 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -85,7 +83,6 @@ public class SoftwareRepository implements XmlRepository {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
 
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             DOMSource source = new DOMSource(doc);
 
             StreamResult file = new StreamResult(new File(fileToWritePath + ".xml"));
